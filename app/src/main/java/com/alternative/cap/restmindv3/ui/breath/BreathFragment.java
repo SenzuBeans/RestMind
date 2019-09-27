9a64ac87bf884a80a8a74a9a6d8704b78aebce05
uppyyoyo <uppyyoyo@gmail.com>
2019-09-27 15:59:15 +0700

    Updated September/27/2019 Part2

--- app/src/main/java/com/alternative/cap/restmindv3/ui/breath/BreathFragment.java
+++ app/src/main/java/com/alternative/cap/restmindv3/ui/breath/BreathFragment.java
@@ -53,6 +53,8 @@ public class BreathFragment extends Fragment {
     }
 
     private void workbench(View rootView, Bundle savedInstanceState) {
+        hideNavigationBar();
+
         circularImageView = rootView.findViewById(R.id.circularImageView);
         playerBtn = rootView.findViewById(R.id.playerBtn);
         breathStatusTextView = rootView.findViewById(R.id.breathStatusTextView);
@@ -99,6 +101,19 @@ public class BreathFragment extends Fragment {
             }
         });
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getActivity().getWindow().getDecorView()
+                .setSystemUiVisibility( View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 
     private void startRunningBreath() {
--- app/src/main/java/com/alternative/cap/restmindv3/ui/setting/SettingFragment.java
+++ app/src/main/java/com/alternative/cap/restmindv3/ui/setting/SettingFragment.java
@@ -45,6 +45,7 @@ public class SettingFragment extends Fragment
     }
 
     private void initInstance(View root, Bundle savedInstanceState) {
+        hideNavigationBar();
         databaseReference = FirebaseDatabase.getInstance().getReference().child("users");
 
         user = FirebaseAuth.getInstance().getCurrentUser();
@@ -84,6 +85,18 @@ public class SettingFragment extends Fragment
             }
         } );
 
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getActivity().getWindow().getDecorView()
+                .setSystemUiVisibility( View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 
     private void swapContent(int status) {
