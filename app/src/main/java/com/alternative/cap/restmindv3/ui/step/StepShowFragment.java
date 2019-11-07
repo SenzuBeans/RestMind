b8cdd2e2b1a5d1dbd3d9280c4fe23db695d394c1
uppyyoyo <uppyyoyo@gmail.com>
2019-11-07 15:45:08 +0700

    11/07/2562

--- app/src/main/java/com/alternative/cap/restmindv3/ui/step/StepShowFragment.java
+++ app/src/main/java/com/alternative/cap/restmindv3/ui/step/StepShowFragment.java
@@ -3,6 +3,7 @@ package com.alternative.cap.restmindv3.ui.step;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.widget.TextViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
